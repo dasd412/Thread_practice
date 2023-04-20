57360ba04754cfaeea702cf8d5f58a6c900a3f84
dasd <dasd412@naver.com>
2023-04-20 12:42:12 +0900

    thread termination practice

---
+++ thread-interrupt/src/main/java/Application.java
@@ -0,0 +1,21 @@
+public class Application {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                Thread.sleep(500000);
+            } catch (InterruptedException e) {
+                System.out.println("Exiting blocking thread");
+            }
+        }
+    }
+}
