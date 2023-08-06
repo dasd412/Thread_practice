c77c323ff7fcaa317c8487f56c76246dc82c41e4
yang-yeongjun <dasd412@naver.com>
2023-08-06 17:42:04 +0900

    스레드 리소스 공유

---
+++ thread-resource-sharing/src/main/java/org/example/Main.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        // 스레드를 각각 순서대로 기다리지 않고, 두 스레드가 모두 종료될 때까지 기다린다. 이렇게 하니 리소스 오염 발생함.
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class IncrementingThread extends Thread {
+
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    public static class DecrementingThread extends Thread {
+
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public void increment() {
+            items++;
+        }
+
+        public void decrement() {
+            items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+}
+
